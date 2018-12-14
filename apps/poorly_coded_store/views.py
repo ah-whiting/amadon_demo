2b9db9ae5ce5c46a5f5aa6847354d0469f2b5fdd
Alex W <runthesun@gmail.com>
2018-12-13 19:18:10 -0800

    server check

--- apps/poorly_coded_store/views.py
+++ apps/poorly_coded_store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
+from django.db.models import Sum
 
 def index(request):
     context = {
@@ -10,7 +11,15 @@ def index(request):
 def checkout(request):
     quantity_from_form = int(request.POST["quantity"])
     price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
     print("Charging credit card...")
+    total_charge = quantity_from_form * price_from_form
     Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    return redirect('/confirmed')
+
+def confirmation(request):
+    context = {
+        'order': Order.objects.last(),
+        'total_cost': Order.objects.aggregate(sum = Sum('total_price')),
+        'total_quantity':Order.objects.aggregate(sum = Sum('quantity_ordered'))
+    }
+    return render(request, "store/checkout.html", context)
